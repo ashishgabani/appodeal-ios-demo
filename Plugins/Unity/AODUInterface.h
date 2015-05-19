43162cba33e247567b8617f51284271bf8a74003
spdd_mac <defaultqp@gmail.com>
2015-05-19 16:48:45 +0800

    fixed unity plugin

---
+++ Plugins/Unity/AODUInterface.h
@@ -0,0 +1,13 @@
+//
+//  AODUInterface.h
+//  iosdemo
+//
+//  Created by Dmitry B on 19.05.15.
+//  Copyright (c) 2015 Appodeal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AODUInterface : NSObject
+
+@end
