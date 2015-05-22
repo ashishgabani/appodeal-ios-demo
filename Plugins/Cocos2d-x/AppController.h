2eae933703e88c257458622c15e64bef1e8f0eb8
spdd_mac <defaultqp@gmail.com>
2015-05-22 13:46:19 +0800

    fixed Cocos2d-x wrapper

---
+++ Plugins/Cocos2d-x/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  iosdemo
+//
+//  Created by Dmitry B on 21.05.15.
+//  Copyright (c) 2015 Appodeal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface AppController : NSObject
+
+@property(nonatomic, strong) UIViewController* viewController;
+
+@end
